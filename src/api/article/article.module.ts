4c7dbacdcdd69a3664e421a7cc34735b82d8564f
babahironobu <ghdtjs0417@gmail.com>
2020-06-27 23:15:16 +0900

    Chor: 오타 수정

--- src/api/article/article.module.ts
+++ src/api/article/article.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ArticleController } from './article.cotroller';
+import { ArticleController } from './article.controller';
 
 @Module({
   controllers: [ArticleController],
